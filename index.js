614700f4e0e93398602de4b1a7dc9f9764ec4479
ACristobal <ajie.io@yahoo.com>
2025-03-02 12:30:13 -0500

    disable the reaction project.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const path = require('path');
 
@@ -6,3 +7,4 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+*/
